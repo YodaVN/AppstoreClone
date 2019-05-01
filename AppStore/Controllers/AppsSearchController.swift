f031924d5c57eaf95edc07ef922869fc7b19eca7
Nhan Nguyen <nhannguyenit.work@gmail.com>
2019-04-30 19:33:26 -0700

    no message

---
+++ AppStore/Controllers/AppsSearchController.swift
@@ -0,0 +1,45 @@
+//
+//  AppsSearchController.swift
+//  AppStore
+//
+//  Created by Nhan Nguyen on 4/29/19.
+//  Copyright © 2019 Nhan Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class AppsSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    private let cellId = "id123"
+    
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .white
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .red
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return .init(width: self.view.frame.width, height: 250)
+    }
+}
